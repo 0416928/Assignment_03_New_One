b4f7a8dc78f33fe4434473c529d2640de9cae896
Gagan <gkaur21.rrc.ca>
2024-10-06 17:55:08 -0500

    Updated interest_update.py by displaying current balances to the console.

--- interest_update.py
+++ interest_update.py
@@ -3,3 +3,15 @@ Description: {Developing a program to process account data to determine interest
 Author: {Gaganpreet Kaur}
 Date: {October 3, 2024}
 """
+
+# IMPORT STATEMENTS
+from pprint import pprint
+
+
+dictionary = {}
+with open("account_balances.txt" , "r") as file:
+    for read_mode in file:
+        key, value = read_mode.strip().split('|')
+        dictionary[key] = float(value)
+
+pprint(dictionary)
